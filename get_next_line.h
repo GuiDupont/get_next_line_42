ceef23847376fa3465b02af7a4339a99ba7e2ccc
gdupont <gdupont.pro@gmail.com>
2020-04-28 12:25:17 +0200

    norm

--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/28 12:24:15 by user42            #+#    #+#             */
+/*   Updated: 2020/04/28 12:24:23 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
 
 char	*ft_strcat_char(char *dest, const char *src, char c);
 int		ft_strlen_char(const char *str, char c);
